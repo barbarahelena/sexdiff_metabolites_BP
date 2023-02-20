48f16f287e3f2bc741f5179d284cd5a65f54bb89
Barbara Verhaar <barbaraverhaar@MacBookBarbara.local>
2023-02-20 22:43:02 +0100

    changed paths, remove unnecessary parts

--- 6d_process_model_results_reg_hrvbrs.R
+++ 6d_process_model_results_reg_hrvbrs.R
@@ -1,6 +1,6 @@
 ## Processing results xBRS / SDNN (HRV)
 
-source("functions.R")
+source("functions_reg.R")
 library(dplyr)
 library(ggplot2)
 library(ggpubr)
@@ -10,25 +10,19 @@ library(ggsci)
 path_true <- file.path('SDNN/Female/output_XGB_reg_SDNN_female_2022_06_03__10-09-11')
 data_path <- file.path('SDNN/Female/input_data')
 
-plot_features_tests_reg(data_path, path_true)
-plot_feature_importance_color(path_true, top_n = 20)
+plot_feature_importance_colors(path_true, top_n = 20)
 
 path_true <- file.path('SDNN/Male/output_XGB_reg_SDNN_male_2022_06_02__18-27-11')
 data_path <- file.path('SDNN/Male/input_data')
 
-plot_features_tests_reg(data_path, path_true)
-plot_feature_importance_color(path_true, top_n = 20)
-
+plot_feature_importance_colors(path_true, top_n = 20)
 
 path_true <- file.path('BRS/Female/output_XGB_reg_BRSFem_2022_03_17__19-30-01')
 data_path <- file.path('BRS/Female/input_data')
 
-plot_features_tests_reg(data_path, path_true)
 plot_feature_importance_color(path_true, top_n = 20)
 
 path_true <- file.path('BRS/Male/output_XGB_reg_BRSMale_2022_03_18__11-14-28')
 data_path <- file.path('BRS/Male/input_data')
 
-plot_features_tests_reg(data_path, path_true)
 plot_feature_importance_color(path_true, top_n = 20)
-
